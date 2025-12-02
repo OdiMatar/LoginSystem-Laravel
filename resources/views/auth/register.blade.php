729ac79112fb0a9a50665a66f5fb81a1b92ebbfe
OdiMatar <340712@student.mboutrecht.nl>
2025-12-02 13:03:02 +0100

    register.blade aangepast

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,7 +28,7 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
+        <!-- Confirm Password 
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
@@ -47,7 +47,7 @@
                             name="rolename" required autocomplete="rolename" />
             
             <x-input-error :messages="$errors->get('rolename')" class="mt-2" />
-        </div>
+        </div> -->
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
